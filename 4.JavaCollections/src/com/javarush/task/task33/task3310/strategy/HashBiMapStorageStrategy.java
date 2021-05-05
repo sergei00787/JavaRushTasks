1e6f9c0e7f9616cc8681bb374de7e34ed7368043
sergei00787 <sergei007_87@mail.ru>
2021-05-05 15:51:31 +0700

    Add task Shotner 13.

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/HashBiMapStorageStrategy.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task33.task3310.strategy;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+
+public class HashBiMapStorageStrategy implements StorageStrategy{
+
+    HashBiMap<Long, String> data = HashBiMap.create();
+
+    @Override
+    public boolean containsKey(Long key) {
+        return data.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(String value) {
+        return data.containsValue(value);
+    }
+
+    @Override
+    public void put(Long key, String value) {
+        data.put(key,value);
+    }
+
+    @Override
+    public Long getKey(String value) {
+        BiMap<String, Long> dataInverse = HashBiMap.create();
+        dataInverse = data.inverse();
+        return dataInverse.get(value);
+    }
+
+    @Override
+    public String getValue(Long key) {
+        return data.get(key);
+    }
+}
