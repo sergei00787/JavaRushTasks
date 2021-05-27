d4534732b37bf66c9f1dd6169120469b6d7ddb42
sergei00787 <sergei007_87@mail.ru>
2021-05-27 13:51:47 +0700

    3.Multithreading Tasks-2.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,72 @@
+package com.javarush.task.task21.task2109;
+
+/* Запрет клонирования
+Разрешите клонировать класс А
+Запретите клонировать класс B
+Разрешите клонировать класс C
+Метод main не участвует в тестировании.
+ */
+
+/* 
+Запретить клонирование
+*/
+
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+
+        @Override
+        protected A clone() throws CloneNotSupportedException {
+            return new A(i, j);
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        protected A clone() throws CloneNotSupportedException {
+            throw new CloneNotSupportedException();
+        }
+    }
+
+    public static class C extends B implements Cloneable{
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        protected C clone() throws CloneNotSupportedException {
+            return new C(getI(),getJ(),getName());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        A a = new A(1,1);
+        //A clonA = a.clone();
+    }
+}
