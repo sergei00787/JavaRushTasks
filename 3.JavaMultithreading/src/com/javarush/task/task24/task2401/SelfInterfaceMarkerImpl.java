99c62797e976b118ae55cf4fbfbdde4b2a794876
sergei00787 <sergei007_87@mail.ru>
2021-06-02 14:37:01 +0700

    3.Multithreading Level 1 Big Tasks 1,2.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker{
+    public boolean isTestMethod(){
+        return true;
+    }
+
+    public void testMethod2(){
+
+    }
+}
