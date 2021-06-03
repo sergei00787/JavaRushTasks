f01b922314abd02b686b91684b52c571357777a6
sergei00787 <sergei007_87@mail.ru>
2021-06-03 15:36:20 +0700

    3.Multithreading Level 4 Tasks 4. And start Big Task MVC(4).

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/FakeModel.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FakeModel implements Model{
+    private ModelData modelData = new ModelData();
+    @Override
+    public ModelData getModelData() {
+        return this.modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = new ArrayList<>();
+
+        users.add(new User("serg", 1L, 21));
+        users.add(new User("serg2", 2L, 22));
+        users.add(new User("serg3", 3L, 23));
+        users.add(new User("serg4", 4L, 24));
+        users.add(new User("serg5", 5L, 25));
+        modelData.setUsers(users);
+    }
+}
