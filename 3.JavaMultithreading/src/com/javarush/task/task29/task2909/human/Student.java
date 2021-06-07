c77696b6eb1a1234e8392e99794b6bcca8dbac3f
sergei00787 <sergei007_87@mail.ru>
2021-06-07 23:38:01 +0700

    3.Multithreading Level 5 Start Big Task Refactoring(6).

---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Student.java
@@ -0,0 +1,63 @@
+package com.javarush.task.task29.task2909.human;
+
+import java.util.Date;
+
+public class Student extends UniversityPerson {
+    private double averageGrade;
+    private int course;
+    private Date beginningOfSession;
+    private Date endOfSession;
+
+    public Student(String name, int age, double averageGrade) {
+        super(name, age);
+        this.name = name;
+        this.age = age;
+        this.averageGrade = averageGrade;
+    }
+
+    public void live() {
+        learn();
+    }
+
+    public void learn() {
+    }
+
+    public String getPosition() {
+        return "Студент";
+    }
+
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void incAverageGradeBy01() {
+        averageGrade += 0.1;
+    }
+
+    public void incAverageGradeBy02() {
+        averageGrade += 0.2;
+    }
+
+    public void setAverageGrade(double averageGrade) {
+        this.averageGrade = averageGrade;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    public void setBeginningOfSession(int day, int month, int year) {
+        beginningOfSession = new Date(year, month, day);
+    }
+
+    public void setEndOfSession(int day, int month, int year) {
+        endOfSession = new Date(year, month, day);
+    }
+
+    public double getAverageGrade() {
+        return averageGrade;
+    }
+
+
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/University.java
@@ -0,0 +1,58 @@
+package com.javarush.task.task29.task2909.human;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class University{
+
+    private List<Student> students;
+    private String name;
+    private int age;
+
+    public University(String name, int age) {
+        students = new ArrayList<>();
+        this.name = name;
+        this.age = age;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Student getStudentWithAverageGrade(double averageGrade){
+        //TODO:
+        for (Student student: students) {
+            if (student.getAverageGrade() >= averageGrade) return student;
+        }
+        return null;
+    }
+
+    public Student getStudentWithMaxAverageGrade() {
+        //TODO:
+        return null;
+    }
+
+    public void getStudentWithMinAverageGradeAndExpel() {
+        //TODO:
+    }
+}
