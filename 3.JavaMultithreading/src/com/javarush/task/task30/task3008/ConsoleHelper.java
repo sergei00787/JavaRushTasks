76fb18f08dacdb9f3242a895cd0be8d00254a780
sergei00787 <sergei007_87@mail.ru>
2021-06-10 16:47:24 +0700

    3.Multithreading Level 6 Big Task Chat 8.

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/ConsoleHelper.java
@@ -0,0 +1,35 @@
+package com.javarush.task.task30.task3008;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ConsoleHelper {
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() {
+        while (true) {
+            try {
+                String result = bufferedReader.readLine();
+                if (result != null) return result;
+            } catch (IOException ex) {
+                writeMessage("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+            }
+        }
+    }
+
+    public static int readInt() {
+        while (true) {
+            try {
+                return Integer.parseInt(readString().trim());
+            } catch (NumberFormatException nex) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+            }
+        }
+    }
+
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -0,0 +1,78 @@
+package com.javarush.task.task30.task3008;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class Server {
+    private static Map<String, Connection> connectionMap = new ConcurrentHashMap<String, Connection>();
+
+    public static void main(String[] args) {
+        ConsoleHelper.writeMessage("Введите порт сервера:");
+        int port = ConsoleHelper.readInt();
+        
+        try(ServerSocket serverSocket = new ServerSocket(port)){
+            ConsoleHelper.writeMessage("Чат сервер запущен.");
+            while(true){
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("Произошла ошибка при запуске или работе сервера.");
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket){
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+
+        }
+
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+
+                Message message = connection.receive();
+                if (message.getType() != MessageType.USER_NAME) {
+                    ConsoleHelper.writeMessage("Получено сообщение от " + socket.getRemoteSocketAddress() + ". Тип сообщения не соответствует протоколу.");
+                    continue;
+                }
+
+                String userName = message.getData();
+
+                if (userName.isEmpty()) {
+                    ConsoleHelper.writeMessage("Попытка подключения к серверу с пустым именем от " + socket.getRemoteSocketAddress());
+                    continue;
+                }
+
+                if (connectionMap.containsKey(userName)) {
+                    ConsoleHelper.writeMessage("Попытка подключения к серверу с уже используемым именем от " + socket.getRemoteSocketAddress());
+                    continue;
+                }
+                connectionMap.put(userName, connection);
+
+                connection.send(new Message(MessageType.NAME_ACCEPTED));
+                return userName;
+            }
+        }
+    }
+
+    public static void sendBroadcastMessage(Message message){
+        try {
+            for (Connection connection: connectionMap.values()) {
+                connection.send(message);
+            }
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Not send message to client");
+        }
+    }
+
+}
